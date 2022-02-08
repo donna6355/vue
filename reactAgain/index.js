2c80dd7c62c8a9108888d26dddd097d58889c0a9
Donna <donnajun1214@gmail.com>
2022-02-08 09:19:35 +0900

    /register post

--- reactAgain/index.js
+++ reactAgain/index.js
@@ -3,6 +3,11 @@ const app = express();
 const port = 5000;
 
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const { User } = require("./models/user");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 mongoose
   .connect(
     "mongodb+srv://donna:toel3173^^@reacttrial.9wq2g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
@@ -22,6 +27,14 @@ mongoose
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
 });
 
 app.listen(port, () => {
